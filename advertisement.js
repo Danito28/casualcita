08e11108afca0e2ff4189f91bb1675f51419ca98
Danito28 <danielfranciscor3@gmail.com>
2017-05-28 19:14:16 +0200

    Create advertisement.js

---
+++ advertisement.js
@@ -0,0 +1,2 @@
+
+document.write('<style type="text/css">#blockblockA {visibility:invisible!important;display:none!important;}#blockblockA td {visibility:invisible!important;display:none!important;}#blockblockA td p {visibility:invisible!important;display:none!important;}#blockblockB {visibility:visible!important;display:block!important;}</style>');
